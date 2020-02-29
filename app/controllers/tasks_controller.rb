5975354d006697ea7b083c3dbd80d20a96245f7b
seishiro.tsukazawa <nantefireda0302@gmail.com>
2020-02-29 07:25:12 +0000

    ruby version change

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+    
     def index
         @tasks = Task.all
     end
@@ -48,6 +49,7 @@ class TasksController < ApplicationController
     end
     
     private
+    
     
     def task_params
         params.require(:task).permit(:content)
